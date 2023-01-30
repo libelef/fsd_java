38154defe39054bb1667d19dfed6b3c82cdb724b
Francisco Libele <francisco.libele@vm.co.mz>
2023-01-30 16:58:27 +0200

    Adding File and Operation  Module

---
+++ src/Implement_OOPS_using_JAVA_with_Data_Structures_and_Beyond/File.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Implement_OOPS_using_JAVA_with_Data_Structures_and_Beyond;
+
+/**
+ * @author libelef
+ *
+ */
+public class File {
+
+	/**
+	 * 
+	 */
+	public File() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Implement_OOPS_using_JAVA_with_Data_Structures_and_Beyond/Operation.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Implement_OOPS_using_JAVA_with_Data_Structures_and_Beyond;
+
+/**
+ * @author libelef
+ *
+ */
+public class Operation {
+
+	/**
+	 * 
+	 */
+	public Operation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
